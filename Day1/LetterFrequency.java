9a53a8c514f0053132830350a4600daf8b30d258
Priyansh Gupta <priyanshu532004@gmail.com>
2025-12-25 14:50:05 +0530

    Code updated of Letter Frequency

--- Day1/LetterFrequency.java
+++ Day1/LetterFrequency.java
@@ -1,14 +1,22 @@
 package day1;
 
 public class LetterFrequency {
+    public static void main(String[] args) {
+        String str = "bab!!";
+        int[] frequency = new int[26];
 
-	public static void main(String[] args) {
-		String s = "bab!!";
-		
-		s.length();
-		
-		s.replace("a","").length();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
 
-	}
+            if (ch >= 'a' && ch <= 'z') {
+                frequency[ch - 'a']++;
+            }
+        }
 
+        for (int i = 0; i < 26; i++) {
+            if (frequency[i] > 0) {
+                System.out.println((char) ('a' + i) + " = " + frequency[i]);
+            }
+        }
+    }
 }
